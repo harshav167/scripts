9c2da83c04cc5ecf46d61cdef8b3623d90c85a43
Harsha Vardhan <80092815+harshav167@users.noreply.github.com>
2024-08-13 16:34:15 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,11 @@ fi
 # Install lazydocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | sudo DIR="/usr/local/bin" bash
 sudo sh -c 'echo "alias lzd=lazydocker" >> /etc/bash.bashrc'
-
+# install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
 # Clean up unused packages
 sudo apt autoremove -y
 
